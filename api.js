fcf522baaf92bf0c88798c94545ee9bd93535b52
Gabil Amanullayev <gabil.mnl@gmail.com>
2025-12-01 18:53:40 +0400

    merge conflicts resolved

---
+++ api.js
@@ -0,0 +1,23 @@
+// api.js
+const apiKey = "8489e45d4dd5537c5ce1862716269412";
+
+export function getWeatherByCity(cityName) {
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
+    return fetch(apiUrl).then(res => {
+        if (!res.ok) throw new Error("City not found");
+        return res.json();
+    });
+}
+
+export function getWeatherByCoords(lat, lon) {
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+    return fetch(apiUrl).then(res => {
+        if (!res.ok) throw new Error("City not found");
+        return res.json();
+    });
+}
+
+export function getCitySuggestions(value, limit = 3) {
+    return fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${value}&limit=${limit}&appid=${apiKey}`)
+        .then(res => res.json());
+}
